85201295a2852dab13ca83799a88168d26a41287
hatab002 <hatab002@umn.edu>
2018-07-02 20:48:11 -0500

    Updates

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import Image from "./components/Image";
+import Score from "./components/Score";
+import gifs from "./gifs.json"
+// import API from "./utils/API";
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    score: 0,
+    topScore: 0,
+    message: "Press any picture to test your memory",
+    gifs
+  }
+
+  // componentDidMount(){
+  //   this.grabGifs("?q=lion+king&limit=10")
+  // }
+
+  // grabGifs = query => {
+  //   API.search(query)
+  //     .then(response =>{
+  //       const gifInfo = response.data;
+  //       const gifs = gifInfo.map(function(){
+  //         return {
+  //           id: gifInfo.images.id,
+  //           src: gifInfo.images.fixed_height_small.url,
+  //           clicked: false
+  //         }
+  //       }).get();
+  //       this.setState({gifs})
+  //     })
+  //     .catch( err => console.log(err))
+  // }
+
+  shuffle = () => {
+    const gifs = this.state.gifs;
+    for (let i = gifs.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const tempGifs = gifs[i];
+      gifs[i] = gifs[j];
+      gifs[j] = tempGifs;
+    }
+    return gifs;
+  }
+
+  clickCheck = id => {
+    const clickedGif = this.state.gifs.filter(gif => gif.id === id);
+    return clickedGif[0].clicked ? true: false;
+  }
+
+  clickingGif = id => {
+    if(this.clickCheck(id)){
+      this.setState({
+        score:0,
+        message: "Nope, you already clicked that one! Game Over! Try again..."
+      });
+      this.state.gifs.map(gif => {
+        gif.clicked = false
+      })
+    } else{
+      const gifs = this.state.gifs.map(gif => {
+        if (gif.id === id){
+          gif.clicked = true;
+        }
+        return gif
+      });
+      this.setState({
+        topScore: this.state.score +1 > this.state.topScore ? this.state.score +1 : this.state.topScore,
+        score: this.state.score + 1,
+        message: "Good one!",
+        gifs: this.shuffle(gifs)
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Score 
+        score={this.state.score}
+        topScore={this.state.topScore}
+        message={this.state.message}
+        />
+        <div className= "row">
+          <div className="imageDiv">
+            {this.state.gifs.map((gifs) => (
+              <Image 
+              key={gifs.id}
+              id={gifs.id}
+              src={gifs.src}
+              clickingGif={this.clickingGif}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Image/Image.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Image.css";
+
+const Image = props => (
+    <div className="col-md-3" onClick={() => props.clickingGif(props.id)}>
+        <div className="giffy">
+            <img className="img img-thumbnail img-fluid" src={props.src} alt="Gif" />
+        </div>
+    </div>
+)
+
+export default Image;
